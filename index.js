2935b18a1662569565bc4617424b8e500d3c7bd7
zesanahmed <zesanahmed593@gmail.com>
2023-06-25 08:56:18 +0600

    banglar rannaghor server

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefData = require('./data/chef.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef is cooking HaHaHa')
 });
 
+app.get('/chefData', (req, res) => {
+    res.send(chefData)
+})
 
 app.listen(port, () => {
     console.log(`Chef API is running on Port: ${port}`);
